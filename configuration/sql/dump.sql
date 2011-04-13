b0e2bc5a84177b1261c9a21d5c84192425ec7256
Christophe Verbinnen <djpate@gmail.com>
2011-04-13 18:58:30 +0200

    new bdd

--- configuration/sql/dump.sql
+++ configuration/sql/dump.sql
@@ -31,8 +31,11 @@ CREATE TABLE `answer` (
   `user` int(11) NOT NULL,
   `accepted` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `question` (`question`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  KEY `question` (`question`),
+  KEY `user` (`user`),
+  CONSTRAINT `answer_ibfk_2` FOREIGN KEY (`user`) REFERENCES `user` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `answer_ibfk_1` FOREIGN KEY (`question`) REFERENCES `question` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,7 +44,40 @@ CREATE TABLE `answer` (
 
 LOCK TABLES `answer` WRITE;
 /*!40000 ALTER TABLE `answer` DISABLE KEYS */;
+INSERT INTO `answer` VALUES (1,2,'2011-04-13 13:33:19','0000-00-00 00:00:00','Il faut faire un sudo reboot ! attention a ton init.d !!',1,0),(6,2,'2011-04-13 17:56:33','0000-00-00 00:00:00','qsdf wfsd gfdgxdfg sdfg wdfg dfg sdf sddfg sdfg sdfg ',1,0);
 /*!40000 ALTER TABLE `answer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `answer_comment`
+--
+
+DROP TABLE IF EXISTS `answer_comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `answer_comment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user` int(11) NOT NULL,
+  `answer` int(11) NOT NULL,
+  `creationDate` datetime NOT NULL,
+  `modificationDate` datetime NOT NULL,
+  `content` text NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user` (`user`),
+  KEY `answer` (`answer`),
+  CONSTRAINT `answer_comment_ibfk_2` FOREIGN KEY (`answer`) REFERENCES `answer` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `answer_comment_ibfk_1` FOREIGN KEY (`user`) REFERENCES `user` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `answer_comment`
+--
+
+LOCK TABLES `answer_comment` WRITE;
+/*!40000 ALTER TABLE `answer_comment` DISABLE KEYS */;
+INSERT INTO `answer_comment` VALUES (1,1,1,'2011-04-13 14:17:04','0000-00-00 00:00:00','Trop bien ta réponse !'),(2,2,1,'2011-04-13 14:27:31','0000-00-00 00:00:00','orem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et enim mi. Ut iaculis nulla dolor. Donec consequat odio quis nibh porttitor porttitor vel nec turpis. Nulla viverra purus congue neque egestas eu gravida velit condimentum. Pellentesque nec magna velit.'),(5,1,1,'2011-04-13 15:21:31','0000-00-00 00:00:00','dsfqsd qsdf qsd f'),(6,1,1,'2011-04-13 15:23:55','0000-00-00 00:00:00','qfgq sdqsdfg qsdfg '),(7,1,1,'2011-04-13 15:25:11','0000-00-00 00:00:00','qsdf qsdf qsd f'),(8,1,1,'2011-04-13 15:25:27','0000-00-00 00:00:00','salult c tro bien ce site'),(9,1,1,'2011-04-13 15:26:26','0000-00-00 00:00:00','hasellus dapibus semper consectetuer hendrerit. '),(10,1,1,'2011-04-13 15:26:42','0000-00-00 00:00:00','Mauris vitae nisl nec metus placerat perdiet est. Phasellus dapibus semper consectetuer hendrerit.Mauris vitae nisl nec metus placerat perdiet est. Phasellus dapibus semper consectetuer hendrerit.Mauris vitae nisl nec metus placerat perdiet est. Phasellus dapibus semper consectetuer hendrerit.Mauris vitae nisl nec metus placerat perdiet est. Phasellus dapibus semper consectetuer hendrerit.'),(11,1,1,'2011-04-13 15:28:03','0000-00-00 00:00:00','gsdfdsgdfgsgdfg sggsb sggfd gsfd'),(12,1,1,'2011-04-13 15:39:03','0000-00-00 00:00:00','qsdfqsdf qdsf qsdf qsd f'),(13,1,1,'2011-04-13 15:39:18','0000-00-00 00:00:00','sdfg sdf gsdf sdf gsdf g'),(14,1,1,'2011-04-13 15:39:23','0000-00-00 00:00:00','sfdgd qsfd gsdfg sdf '),(15,1,1,'2011-04-13 15:39:34','0000-00-00 00:00:00','qsdf qsd fqsd'),(16,1,1,'2011-04-13 15:40:19','0000-00-00 00:00:00','qsd fqsdf qsd fqdsf qdsf qsdf ');
+/*!40000 ALTER TABLE `answer_comment` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -56,8 +92,11 @@ CREATE TABLE `answer_vote` (
   `user` int(11) NOT NULL,
   `value` tinyint(1) NOT NULL,
   `date` datetime NOT NULL,
-  PRIMARY KEY (`answer`,`user`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`answer`,`user`),
+  KEY `user` (`user`),
+  CONSTRAINT `answer_vote_ibfk_2` FOREIGN KEY (`user`) REFERENCES `user` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `answer_vote_ibfk_1` FOREIGN KEY (`answer`) REFERENCES `answer` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -84,8 +123,10 @@ CREATE TABLE `question` (
   `content` text NOT NULL,
   `user` int(11) NOT NULL,
   `views` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id`),
+  KEY `user` (`user`),
+  CONSTRAINT `question_ibfk_1` FOREIGN KEY (`user`) REFERENCES `user` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -94,7 +135,40 @@ CREATE TABLE `question` (
 
 LOCK TABLES `question` WRITE;
 /*!40000 ALTER TABLE `question` DISABLE KEYS */;
+INSERT INTO `question` VALUES (2,'2011-04-13 13:27:26','1970-01-01 01:00:00','Mon serveur unix ne démarre plus !','Mauris vitae nisl nec metus placerat perdiet est. Phasellus dapibus semper consectetuer hendrerit.',2,146);
 /*!40000 ALTER TABLE `question` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `question_comment`
+--
+
+DROP TABLE IF EXISTS `question_comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `question_comment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user` int(11) NOT NULL,
+  `question` int(11) NOT NULL,
+  `creationDate` datetime NOT NULL,
+  `modificationDate` datetime NOT NULL,
+  `content` text NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `question` (`question`),
+  KEY `user` (`user`),
+  CONSTRAINT `question_comment_ibfk_2` FOREIGN KEY (`user`) REFERENCES `user` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `question_comment_ibfk_1` FOREIGN KEY (`question`) REFERENCES `question` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `question_comment`
+--
+
+LOCK TABLES `question_comment` WRITE;
+/*!40000 ALTER TABLE `question_comment` DISABLE KEYS */;
+INSERT INTO `question_comment` VALUES (8,1,2,'2011-04-13 18:04:12','0000-00-00 00:00:00','sfdg qqdsf qsdf qsdf ');
+/*!40000 ALTER TABLE `question_comment` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -109,8 +183,10 @@ CREATE TABLE `question_tag` (
   `tag` int(11) NOT NULL,
   PRIMARY KEY (`question`,`tag`),
   KEY `question` (`question`),
-  KEY `tag` (`tag`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  KEY `tag` (`tag`),
+  CONSTRAINT `question_tag_ibfk_2` FOREIGN KEY (`tag`) REFERENCES `tag` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `question_tag_ibfk_1` FOREIGN KEY (`question`) REFERENCES `question` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -119,6 +195,7 @@ CREATE TABLE `question_tag` (
 
 LOCK TABLES `question_tag` WRITE;
 /*!40000 ALTER TABLE `question_tag` DISABLE KEYS */;
+INSERT INTO `question_tag` VALUES (2,2),(2,6),(2,7);
 /*!40000 ALTER TABLE `question_tag` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -134,8 +211,11 @@ CREATE TABLE `question_vote` (
   `user` int(11) NOT NULL,
   `value` tinyint(1) NOT NULL,
   `date` datetime NOT NULL,
-  PRIMARY KEY (`question`,`user`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`question`,`user`),
+  KEY `user` (`user`),
+  CONSTRAINT `question_vote_ibfk_2` FOREIGN KEY (`user`) REFERENCES `user` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `question_vote_ibfk_1` FOREIGN KEY (`question`) REFERENCES `question` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -144,6 +224,7 @@ CREATE TABLE `question_vote` (
 
 LOCK TABLES `question_vote` WRITE;
 /*!40000 ALTER TABLE `question_vote` DISABLE KEYS */;
+INSERT INTO `question_vote` VALUES (2,1,1,'2011-04-13 13:59:48');
 /*!40000 ALTER TABLE `question_vote` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -158,7 +239,7 @@ CREATE TABLE `tag` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `label` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -167,7 +248,34 @@ CREATE TABLE `tag` (
 
 LOCK TABLES `tag` WRITE;
 /*!40000 ALTER TABLE `tag` DISABLE KEYS */;
+INSERT INTO `tag` VALUES (1,'PHP'),(2,'MySQL'),(3,'ASP'),(4,'Oracle'),(5,'Windows'),(6,'Linux'),(7,'Unix');
 /*!40000 ALTER TABLE `tag` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'djpate','toto','djpate@gmail.com'),(2,'korben','toto','gaultier.monier@gmail.com');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -179,4 +287,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2011-04-11 12:17:08
+-- Dump completed on 2011-04-13 18:58:25
